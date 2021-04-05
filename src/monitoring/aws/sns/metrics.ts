ba69fc2dceec5709152aab4b9c74dbd61d8d0193
Vojta Hordějčuk 🦉 <vojta@amazon.com>
2021-04-05 21:57:45 +0200

    feat: add metrics for redshift/sns/sqs

---
+++ src/monitoring/aws/sns/metrics.ts
@@ -0,0 +1,40 @@
+import { Metric, Statistic } from '@aws-cdk/aws-cloudwatch';
+import { Duration } from '@aws-cdk/core';
+
+const enum Metrics {
+  NumberOfMessagesPublished = 'NumberOfMessagesPublished',
+  NumberOfNotificationsDelivered = 'NumberOfNotificationsDelivered',
+  NumberOfNotificationsFailed = 'NumberOfNotificationsFailed',
+  PublishSize = 'PublishSize'
+}
+
+const Namespace = 'AWS/SNS';
+
+export class SnsMetricFactory {
+  metricNumberOfMessagesPublished(topicName: string) {
+    return this.metric(Metrics.NumberOfMessagesPublished, topicName).with({ statistic: Statistic.SUM });
+  }
+
+  metricNumberOfMessagesDelivered(topicName: string) {
+    return this.metric(Metrics.NumberOfNotificationsDelivered, topicName).with({ statistic: Statistic.SUM });
+  }
+
+  metricNumberOfNotificationsFailed(topicName: string) {
+    return this.metric(Metrics.NumberOfNotificationsFailed, topicName).with({ statistic: Statistic.SUM });
+  }
+
+  metricAverageMessageSizeInBytes(topicName: string) {
+    return this.metric(Metrics.PublishSize, topicName).with({ statistic: Statistic.AVERAGE });
+  }
+
+  protected metric(metric: Metrics, topicName: string) {
+    return new Metric({
+      metricName: metric,
+      namespace: Namespace,
+      period: Duration.minutes(5),
+      dimensions: {
+        TopicName: topicName,
+      },
+    });
+  }
+}
