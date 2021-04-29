56b4958864b8342b0a748066e3ad47e8a9c17123
John Corrales <john.corrales@helix.com>
2021-04-29 10:00:43 -0700

    bumped (#25)
    * bumped
    
    * ran again

--- .projenrc.js
+++ .projenrc.js
@@ -17,7 +17,7 @@ const project = new AwsCdkConstructLibrary({
   // creates PRs for projen upgrades
   projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
 
-  cdkVersion: '1.100.0',
+  cdkVersion: '1.101.0',
   cdkVersionPinning: true,
   cdkDependencies: [
     '@aws-cdk/aws-apigateway',
