86c6b2c95ee0774ab4dfb9016eb4f2a84d3fc9bc
mark-ship-it <72764546+mark-ship-it@users.noreply.github.com>
2021-05-11 10:47:24 -0700

    1.103.0 release (#28)

--- .projenrc.js
+++ .projenrc.js
@@ -17,7 +17,7 @@ const project = new AwsCdkConstructLibrary({
   // creates PRs for projen upgrades
   projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
 
-  cdkVersion: '1.102.0',
+  cdkVersion: '1.103.0',
   cdkVersionPinning: true,
   cdkDependencies: [
     '@aws-cdk/aws-apigateway',
