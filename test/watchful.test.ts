e1511564042a967b5b297356981cacf520857026
Elad Ben-Israel <benisrae@amazon.com>
2021-05-20 15:46:28 +0300

    feat: alarm action ARNs
    Allow specifying arbitrary string ARNs as alarm actions in watchful.
    
    This also fixes #474 which was a bug in the DynamoDB watchful logic which did not take into account the fact that the billing mode is undefined if it is set to PROVISIONED (because that's the default).

--- test/watchful.test.ts
+++ test/watchful.test.ts
@@ -1,4 +1,5 @@
 import { expect as cdk_expect, haveResource } from '@aws-cdk/assert';
+import * as ddb from '@aws-cdk/aws-dynamodb';
 import { Stack } from '@aws-cdk/core';
 import { Watchful } from '../src';
 
@@ -13,3 +14,28 @@ test('creates an empty dashboard', () => {
   cdk_expect(stack).to(haveResource('AWS::CloudWatch::Dashboard'));
 });
 
+test('alarmActions can be used to specify a list of custom alarm actions', () => {
+  // GIVEN
+  const stack = new Stack();
+  const table = new ddb.Table(stack, 'Table', {
+    partitionKey: { name: 'ID', type: ddb.AttributeType.STRING },
+  });
+
+  // WHEN
+  const wf = new Watchful(stack, 'watchful', {
+    alarmActionArns: [
+      'arn:of:custom:alarm:action',
+      'arn:2',
+    ],
+  });
+
+  wf.watchDynamoTable('MyTable', table);
+
+  // THEN
+  cdk_expect(stack).to(haveResource('AWS::CloudWatch::Alarm', {
+    AlarmActions: [
+      'arn:of:custom:alarm:action',
+      'arn:2',
+    ],
+  }));
+});
